b1798f717b187d83832c2ea2921340f2e4973993
minhhiu <leminhhieu0937659254@gmail.com>
2022-11-04 13:51:10 +0700

    fix Netlify

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://pacific-meadow-62769.herokuapp.com/api'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://pacific-meadow-62769.herokuapp.com/api' : ''
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit - mern'
 
